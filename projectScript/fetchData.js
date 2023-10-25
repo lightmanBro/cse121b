3e0954198344e4a51f04104db2c4e712eba8891e
LightmanBro <omotosodavidromeo@gmail.com>
2023-10-25 15:48:32 +0100

    fixed bug

--- projectScript/fetchData.js
+++ projectScript/fetchData.js
@@ -12,77 +12,56 @@ const getBooks = async function (bookName) {
     books = await response.json();
     handleResponse(books.items);
   } catch (error) {
-    console.error("error", error.message);
+    setTimeout(() => {
+      console.error("error", error.message);
+    }, 200);
   }
 };
+
 const book = document.querySelector("#name");
 document.querySelector(".proceed").addEventListener("click", () => {
   getBooks(book.value);
-  document.querySelector('.results').textContent = "";
+  document.querySelector(".results").textContent = "";
 });
 
 function handleResponse(response) {
-  response.forEach((element) => {
-    console.log(element);
-    let bookHtml = `<div class="books">
-    <div class="book-title">${element.volumeInfo.title}</div>
-    <img src="${element.volumeInfo.imageLinks.thumbnail ? element.volumeInfo.imageLinks.thumbnail : ""}" alt="${element.volumeInfo.title} image">
-    <div class="rest">
-        <div class="author">Author ${element.volumeInfo.authors[0]}</div>
-        <div class="published">Published ${element.volumeInfo.publishedDate}</div>
-        <div class="page-count">Pages ${element.volumeInfo.pageCount}</div>
-        <a href="${element.volumeInfo.infoLink}">Learn More....</a>
-    </div>
-</div>`
-    document.querySelector('.results').insertAdjacentHTML('afterbegin',bookHtml);
-  });
+  found(response);
 }
-//   imageLink.thumbnail
 
-/*
-allowAnonLogging
-: 
-false
-authors
-: 
-['GLADYS SCHMITT']
-canonicalVolumeLink
-: 
-"https://books.google.com/books/about/DAVID_THE_KING.html?hl=&id=pfRGuwEACAAJ"
-contentVersion
-: 
-"0.3.2.0.preview.0"
-imageLinks
-: 
-{smallThumbnail: 'http://books.google.com/books/content?id=pfRGuwEAC…J&printsec=frontcover&img=1&zoom=5&source=gbs_api', thumbnail: 'http://books.google.com/books/content?id=pfRGuwEAC…J&printsec=frontcover&img=1&zoom=1&source=gbs_api'}
-infoLink
-: 
-"http://books.google.com/books?id=pfRGuwEACAAJ&dq=David&hl=&source=gbs_api"
-language
-: 
-"en"
-maturityRating
-: 
-"NOT_MATURE"
-pageCount
-: 
-642
-panelizationSummary
-: 
-{containsEpubBubbles: false, containsImageBubbles: false}
-previewLink
-: 
-"http://books.google.com/books?id=pfRGuwEACAAJ&dq=David&hl=&cd=7&source=gbs_api"
-printType
-: 
-"BOOK"
-publishedDate
-: 
-"1946"
-readingModes
-: 
-{text: false, image: false}
-title
-: 
-"DAVID THE KING"
-*/
+function found(available) {
+  switch (available) {
+    case available:
+      available.forEach((element) => {
+        let bookHtml = `<div class="books">
+                <div class="book-title">${element.volumeInfo.title}</div>
+                <img src="${
+                  element.volumeInfo.imageLinks.thumbnail
+                    ? element.volumeInfo.imageLinks.thumbnail
+                    : ""
+                }" alt="${element.volumeInfo.title} image">
+                <div class="rest">
+                    <div class="author">Author ${
+                      element.volumeInfo.authors[0]
+                    }</div>
+                    <div class="published">Published ${
+                      element.volumeInfo.publishedDate
+                    }</div>
+                    <div class="page-count">Pages ${
+                      element.volumeInfo.pageCount
+                    }</div>
+                    <a href="${element.volumeInfo.infoLink}">Learn More....</a>
+                </div>
+            </div>`;
+        document
+          .querySelector(".results")
+          .insertAdjacentHTML("afterbegin", bookHtml);
+      });
+      break;
+    case !available:
+      document.querySelector(".results").innerHTML =
+        "<h1>Not found Please Try another Keyword<h2>";
+      break;
+    default:
+      break;
+  }
+}
